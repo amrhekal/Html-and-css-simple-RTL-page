9a537607422b4462425e224295c18454698ea23a
AmrHekal <amrhekal2030@gmail.com>
2024-08-19 11:44:40 +0300

    My First Commit in This Painful Task

---
+++ js/script.js
@@ -0,0 +1,3 @@
+// // Set Main Header Height
+// const mainNav = document.getElementById('mainNav').offsetHeight;
+// document.getElementById('mainHeader').style.height = `calc(100vh - ${mainNav}px)`;
